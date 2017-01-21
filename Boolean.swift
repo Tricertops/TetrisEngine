91ace78b6485dbc2daea815e394aefc741d87cd6
Martin Kiss <martin.kiss@me.com>
2017-01-21 11:27:51 +0100

    Random booleans

--- Boolean.swift
+++ Boolean.swift
@@ -5,6 +5,7 @@
 //  Created by Martin Kiss on 17 Jan 2017.
 //  Copyright © 2017 Tricertops. All rights reserved.
 //
+import Foundation
 
 let yes = true
 let no = false
@@ -15,4 +16,7 @@ extension Bool {
         return !self
     }
     
+    static func random() -> Bool {
+        return (arc4random_uniform(2) == 0)
+    }
 }
